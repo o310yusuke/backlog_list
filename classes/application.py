535439d7fd7642a0e5eaea4407d6e03ec7b407ea
o310yusuke <yusuke310@gmail.com>
2020-05-27 01:39:42 +0900

    アプリケーションタイトルの追加

--- classes/application.py
+++ classes/application.py
@@ -1,15 +1,28 @@
 # coding: utf-8
 
 import tkinter as tk
+from tkinter import font
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, title=None):
         super().__init__(master)
         self.master = master
+        self.title = title
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
+        self.master.title(self.title)
+        
+        # アプリタイトルのフォント設定
+        font_title = font.Font(size=14, weight='bold', underline=1)
+        label_title = tk.Label(
+            self.master,
+            text=self.title,
+            font=font_title
+        )
+        label_title.pack(side='top')
+
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
@@ -24,5 +37,6 @@ class Application(tk.Frame):
 
 
 root = tk.Tk()
-app = Application(master=root)
+root.geometry('400x300')
+app = Application(master=root, title=u'Backlog期限切れチケット抽出ツール')
 app.mainloop()
