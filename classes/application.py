cae77aff7acfe38ff205b3862c38d5b8b6607d7d
o310yusuke <yusuke310@gmail.com>
2020-05-30 11:58:15 +0900

    レイアウトは無視してパーツを配置

--- classes/application.py
+++ classes/application.py
@@ -2,6 +2,7 @@
 
 import tkinter as tk
 from tkinter import font
+from tkinter import ttk
 
 class Application(tk.Frame):
     def __init__(self, master=None, title=None):
@@ -9,34 +10,112 @@ class Application(tk.Frame):
         self.master = master
         self.title = title
         self.pack()
-        self.create_widgets()
+        self.application_settings()
+        self.call_widgets()
 
-    def create_widgets(self):
+    def application_settings(self):
+        # アプリヘッダにタイトルを表示
         self.master.title(self.title)
-        
+        # アプリ全体のフォントサイズを指定
+        self.master.option_add('*font', font.Font(size=10))
+
+    # master上に各フレームを配置
+    def call_widgets(self):
+        self.create_header()
+        self.create_setting()
+        self.create_result()
+
+    def create_header(self):
+        header_frame = tk.Frame(self.master)
+        header_frame.pack(anchor=tk.W, padx=2)
+
         # アプリタイトルのフォント設定
         font_title = font.Font(size=14, weight='bold', underline=1)
         label_title = tk.Label(
-            self.master,
+            header_frame,
             text=self.title,
             font=font_title
         )
-        label_title.pack(side='top')
+        label_title.pack(side='top', anchor=tk.W)
 
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+    def create_setting(self):
+        setting_frame = tk.Frame(self.master)
+        setting_frame.pack(anchor=tk.W, padx=2, pady=5)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        area_title = tk.Label(setting_frame, text=u'◆設定◆')
+        area_title.pack(anchor=tk.W)
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        label_target_pj = tk.Label(setting_frame, text=u'対象PJID：')
+        entry_target_pj = tk.Entry(setting_frame, width=10)
+        # label_target_pj.pack(side='left')
+        label_target_pj.pack()
+        entry_target_pj.pack()
+
+        label_connecting_way = tk.Label(setting_frame, text=u'接続方法：')
+        label_connecting_way.pack()
+
+        # APIKeyパターン
+        checkbox_apikey = tk.Checkbutton(
+            setting_frame, text=u'API Key', command=None
+        )
+        checkbox_apikey.pack(anchor=tk.W)
+        # labelframe_apikey = tk.LabelFrame(setting_frame, labelwidget=checkbox_apikey)
+        # labelframe_apikey.pack(anchor=tk.W)
+        entry_apikey = tk.Entry(setting_frame, width=20)
+        entry_apikey.pack(anchor=tk.W)
+
+        # ID/PWパターン
+        checkbox_idpw = tk.Checkbutton(
+            setting_frame, text=u'ID/PW', command=None
+        )
+        checkbox_idpw.pack(anchor=tk.W)
+        label_id = tk.Label(setting_frame, text=u'ID：')
+        entry_id = tk.Entry(setting_frame, width=10)
+        label_pw = tk.Label(setting_frame, text=u'PW：')
+        entry_pw = tk.Entry(setting_frame, width=20)
+        label_id.pack()
+        entry_id.pack()
+        label_pw.pack()
+        entry_pw.pack()
+
+        # ボタン配置
+        button_search = tk.Button(text=u'検索', command=None)
+        button_search.pack()
+
+        button_exit = tk.Button(text=u'終了', command=None)
+        button_exit.pack()
+
+    def create_result(self):
+        result_frame = tk.Frame(self.master)
+        result_frame.pack(anchor=tk.W, padx=2, pady=5)
+
+        area_title = tk.Label(result_frame, text=u'◆検索結果◆')
+        area_title.pack(anchor=tk.W)
+
+        label_result_count = tk.Label(result_frame, text=u'合計：' + u'件')
+        label_result_count.pack()
+
+        # 検索結果テーブル
+        treeview_result = ttk.Treeview(result_frame)
+        treeview_result['columns'] = (1,2,3,4,5)
+        treeview_result['show'] = 'headings'
+        treeview_result.heading(1, text=u'チケット番号')
+        treeview_result.heading(2, text=u'タイトル')
+        treeview_result.heading(3, text=u'期限')
+        treeview_result.heading(4, text=u'担当者')
+        treeview_result.heading(5, text=u'作成者')
+
+        treeview_result.pack()
+
+        # test data
+        treeview_result.insert('', 'end', 
+            values=('PJ-NAME_0001', '【インフラ】テスト環境構築', '2020/04/05', '太郎', '花子')
+        )
+
+
 
 
 root = tk.Tk()
-root.geometry('400x300')
+root.geometry('500x500')
 app = Application(master=root, title=u'Backlog期限切れチケット抽出ツール')
 app.mainloop()
